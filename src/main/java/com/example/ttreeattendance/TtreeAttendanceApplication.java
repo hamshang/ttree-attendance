a76588a61e8b5c5dd6be1e4a1d334135bba10ab4
shham <shham@anchorwave.co.kr>
2026-06-30 12:34:59 +0900

    Initial commit

---
+++ src/main/java/com/example/ttreeattendance/TtreeAttendanceApplication.java
@@ -0,0 +1,13 @@
+package com.example.ttreeattendance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TtreeAttendanceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TtreeAttendanceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ttreeattendance/TtreeAttendanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ttreeattendance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TtreeAttendanceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
